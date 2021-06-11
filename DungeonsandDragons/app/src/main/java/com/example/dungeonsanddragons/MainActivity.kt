125cf565563af85d9d37bb595c0bfb1b0dd02c3e
RileyKenyon <rike2277@colorado.edu>
2021-06-10 20:47:21 -0600

    Added TAG for debugging, made parsing more reliable

--- DungeonsandDragons/app/src/main/java/com/example/dungeonsanddragons/MainActivity.kt
+++ DungeonsandDragons/app/src/main/java/com/example/dungeonsanddragons/MainActivity.kt
@@ -11,9 +11,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 
-open class MainActivity : AppCompatActivity() {
+private const val TAG = "MainApplication"
+
+class MainActivity : AppCompatActivity() {
     // List of internal records from NFC read
-    private lateinit var currentNdefRecords: List<NdefRecord>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,21 +24,5 @@ open class MainActivity : AppCompatActivity() {
         val imageView: ImageView = findViewById(R.id.title_image)
         Glide.with(this).load(R.drawable.title).into(imageView)
 
-        // Check if the NFC adapter triggered the application launch
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action) {
-            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
-                val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
-                currentNdefRecords = messages.flatMap { it.records.asList() }
-
-                // Read the payload and convert to a UTF8 string
-                val textView: TextView = findViewById(R.id.nfc_data)
-                textView.text = getCurrentRecordText()
-            }
-        }
-    }
-
-    private fun getCurrentRecordText() : String {
-        // Return most recent NFC records
-        return currentNdefRecords[0].payload.toString(Charsets.UTF_8)
     }
 }
--- DungeonsandDragons/app/src/main/java/com/example/dungeonsanddragons/NFC.kt
+++ DungeonsandDragons/app/src/main/java/com/example/dungeonsanddragons/NFC.kt
@@ -1,10 +1,11 @@
 package com.example.dungeonsanddragons
 
 import android.app.Activity
+import android.app.Service
 import android.content.Intent
 import android.nfc.NdefMessage
 import android.nfc.NdefRecord
-import android.nfc.NdefRecord.TNF_ABSOLUTE_URI
+import android.nfc.NdefRecord.*
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.provider.Settings
@@ -13,21 +14,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import androidx.fragment.app.FragmentActivity
 
-class NFC : FragmentActivity() {
+private const val TAG = "NFC"
 
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
+class NFC : AppCompatActivity(){
+    // List of internal records from NFC read
+    private lateinit var currentNdefMessages: List<NdefMessage>
+    private lateinit var currentNdefRecords: List<NdefRecord>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // set the content view as the NFC layout
+        setContentView(R.layout.nfc)
+        // Check if the NFC adapter triggered the application launch
         if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action) {
             intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
-                val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
-                // process message array
-                val messageRecord = messages[0].records[0]
-                val nfcData = messageRecord.payload.toString()
+                currentNdefMessages = rawMessages.map { it as NdefMessage }
+                currentNdefRecords = currentNdefMessages.flatMap { it.records.asList() }
+
+                // Read the payload and convert to a UTF8 string
+                val textView: TextView = findViewById(R.id.nfc_data)
+                textView.text = getCurrentRecordText()
             }
         }
-//        getContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.nfc_data)
-        textView.text = "Scanned NFC"
-        Log.w("NFC","Scanned NFC Tag")
+    }
+
+    private fun getCurrentRecordText() : String {
+        // Return most recent NFC records
+
+        // Check NFC message type TNF (3 bit Type Name Field)
+        val payload : String = when (currentNdefRecords[0].tnf) {
+            TNF_MIME_MEDIA -> currentNdefRecords[0].payload.toString(Charsets.UTF_8)
+            TNF_WELL_KNOWN -> currentNdefRecords[0].payload.toString(Charsets.UTF_8)
+            else -> ""
+        }
+//        Log.d(TAG + "_TNF",currentNdefRecords[0].tnf.toString())
+        Log.d(TAG,currentNdefRecords[0].id.joinToString(Charsets.))
+        return payload
+    }
+
+    private fun getCurrentRecordId() : String {
+        // Return most recent NFC records
+        return currentNdefRecords[0].id.toString(Charsets.UTF_8)
     }
 }
