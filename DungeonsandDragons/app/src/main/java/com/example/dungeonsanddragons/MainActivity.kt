142f677c6354e6d21782fc365ff99d24e348aa47
RileyKenyon <rike2277@colorado.edu>
2021-11-22 10:27:36 -0700

    WIP migrating main activity to use firbase auth

--- DungeonsandDragons/app/src/main/java/com/example/dungeonsanddragons/MainActivity.kt
+++ DungeonsandDragons/app/src/main/java/com/example/dungeonsanddragons/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dungeonsanddragons
 
+import android.content.Intent
 import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.nfc.Tag
@@ -18,13 +19,24 @@ import com.example.dungeonsanddragons.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 private const val TAG = "MainApplication"
 
 class MainActivity : AppCompatActivity() {
 
+    // Navigation instance variables
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    // Firebase instance variables
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,21 +55,60 @@ class MainActivity : AppCompatActivity() {
         // Alternate toolbar setup
         findViewById<MaterialToolbar>(R.id.topAppBar).setupWithNavController(navController,appBarConfiguration)
 
-        // Check if the NFC adapter triggered the application launch
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action) {
-            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
-                val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-                val messages = rawMessages.map { it as NdefMessage }
-                val bundle = bundleOf(
-                    "rawMessages" to messages,
-                    "id" to tag?.id
-                )
-                navController.navigate(R.id.action_startup_to_NFC, bundle)
-            }
+        // Check for debug mode of the firebase
+        // "10.0.2.2" is a special value which allows the Android emulator to
+        // connect to "localhost" on the host computer. The port values are
+        // defined in the firebase.json file.
+        if (BuildConfig.DEBUG) {
+            Firebase.database.useEmulator("10.0.2.2", 9000)
+            Firebase.auth.useEmulator("10.0.2.2", 9099)
+            Firebase.storage.useEmulator("10.0.2.2", 9199)
         }
+
+        // Check if user is signed in
+        val user = Firebase.auth.currentUser
+        if (user == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(Intent(this, Signup::class.java))
+//            navController.navigate(R.id.startup)
+//            finish()
+//            return
+        }
+
+        // Initialize Realtime Database
+//        db = Firebase.database
+//        val messagesRef = db.reference.child(MESSAGES_CHILD)
+
+        // Check if the NFC adapter triggered the application launch
+//        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action) {
+//            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
+//                val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+//                val messages = rawMessages.map { it as NdefMessage }
+//                val bundle = bundleOf(
+//                    "rawMessages" to messages,
+//                    "id" to tag?.id
+//                )
+//                navController.navigate(R.id.action_startup_to_NFC, bundle)
+//            }
+//        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener{
+                Log.d(Signup.TAG,"Signed out successfully")
+            }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        const val MESSAGES_CHILD = "messages"
+        const val ANONYMOUS = "anonymous"
+        private const val LOADING_IMAGE_URL = "https://www.google.com/images/spin-32.gif"
     }
 }
--- DungeonsandDragons/app/src/main/java/com/example/dungeonsanddragons/Signup.kt
+++ DungeonsandDragons/app/src/main/java/com/example/dungeonsanddragons/Signup.kt
@@ -1,11 +1,13 @@
 package com.example.dungeonsanddragons
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.dungeonsanddragons.databinding.SignUpBinding
@@ -13,16 +15,18 @@ import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 private const val TAG = "SignInApplication"
 
-abstract class Signup : AppCompatActivity() {
+class Signup : AppCompatActivity() {
     private var _binding: SignUpBinding? = null
     private lateinit var textView: TextView
     private val binding get() = _binding!!
 
     // Firebase configuration
-    private val signInLauncher = registerForActivityResult(
+    private val signIn = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
     ) { res ->
         this.onSignInResult(res)
@@ -30,7 +34,67 @@ abstract class Signup : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseAuth.getInstance().useEmulator('10.0.2.2', 9099);
+        _binding = SignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        // Check if user is signed in - launch Firebase UI
+        // Otherwise proceed to mainActivity
+        if (Firebase.auth.currentUser == null) {
+            // Sign in with the FirebaseUI
+            createSignInIntent()
+        } else {
+            goToMainActivity()
+        }
+
+    }
+    private fun goToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // successfully signed in
+            Log.d(TAG,"Sign in successful")
+            var user = FirebaseAuth.getInstance().currentUser
+        } else {
+            // Sign in failed
+            Toast.makeText(
+                this,
+                "There was an error signing in",
+                Toast.LENGTH_LONG).show()
+
+            val response = result.idpResponse
+            if (response == null) {
+                Log.w(TAG, "Sign in canceled")
+            } else {
+                Log.w(TAG, "Sign in error", response.error)
+            }
+        }
+    }
+
+    private fun createSignInIntent() {
+        // Authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signIn.launch(signInIntent)
+    }
+
+    companion object {
+        const val TAG = "SignUpActivity"
     }
 //
 //    override fun onCreateView(
@@ -50,44 +114,13 @@ abstract class Signup : AppCompatActivity() {
 //        _binding = null
 //    }
 
-    private fun createSignInIntent() {
-        // Authentication providers
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build()
-        )
-
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        signInLauncher.launch(signInIntent)
-    }
-
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // successfully signed in
-            var user = FirebaseAuth.getInstance().currentUser
-        } else {
-            // Sign in failed
-        }
-    }
-     private fun signOut() {
-         AuthUI.getInstance()
-             .signOut(this)
-             .addOnCompleteListener{
-                 Log.d(TAG,"Signed out successfully")
-             }
-     }
-
-    private fun delete() {
-        AuthUI.getInstance()
-            .delete(this)
-            .addOnCompleteListener{
-                Log.d(TAG,"Deleted successfully")
-            }
-    }
+//
+//    private fun delete() {
+//        AuthUI.getInstance()
+//            .delete(this)
+//            .addOnCompleteListener{
+//                Log.d(TAG,"Deleted successfully")
+//            }
+//    }
 
 }
