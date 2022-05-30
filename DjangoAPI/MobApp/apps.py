a22d0616c2fe0451c0ddd608878a4ef20e0985ae
RileyKenyon <rike2277@colorado.edu>
2022-05-30 12:37:02 -0600

    removed Django API

--- DjangoAPI/MobApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MobappConfig(AppConfig):
-    name = 'MobApp'
--- DjangoAPI/MobApp/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-from django.views.decorators.csrf import csrf_except
-from rest_framework.parsers import JSONParser
-
-# Create your tests here.
