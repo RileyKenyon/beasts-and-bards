091d00b63de95e72887aaaeeba87dfb67b23048a
RileyKenyon <rike2277@colorado.edu>
2022-05-30 18:28:53 -0600

    First pass at adding bottom navigation bar
      - added icons for each task
      - character sheet
      - inventory
      - monster manual
      - Map

---
+++ android/app/src/main/java/com/beastsandbards/android/QuestFragment.kt
@@ -0,0 +1,99 @@
+package com.example.dungeonsanddragons
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.dungeonsanddragons.databinding.FragmentQuestBinding
+import com.example.dungeonsanddragons.databinding.NavHeaderBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [QuestFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class QuestFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    // Add bindings
+    private var _binding: FragmentQuestBinding? = null
+    private val binding get() = _binding!!
+
+    // Navigation
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+        // Setup navigation
+        navController = findNavController()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentQuestBinding.inflate(inflater, container, false)
+
+        // Setup action bar
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.character_sheet -> {
+                    Log.d(TAG,"Character sheet")
+                    true
+                }
+                R.id.inventory -> {
+                    Log.d(TAG,"Inventory")
+                    true
+                }
+                R.id.monster_manual -> {
+                    Log.d(TAG,"Monster Manual")
+                    true
+                }
+                R.id.map -> {
+                    Log.d(TAG,"Map")
+                    true
+                }
+                else -> false
+            }
+        }
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment quest.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            QuestFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+
+        private const val TAG = "QUEST"
+    }
+}
